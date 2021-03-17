b933e28da94b28dda02543039dc724d72fe6e00d
Vitor Hardoim <vitorhardoim@id.uff.br>
2021-03-17 20:17:03 -0300

    update: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,7 @@ let publicPath = process.env.NODE_ENV === 'production' ? 'light-blue-vue-admin/'
 module.exports = {
   publicPath,
   productionSourceMap: false,
+  devServer: {
+    public: 'painel.brat.com.br:80'
+  }
 };
